2cd2dbc3852bf229579f7554a451267f94af0fa4
mosina003 <smosina003@gmail.com>
2026-01-31 00:02:04 +0530

    Update Day13 Main.java

--- Day13/Main.java
+++ Day13/Main.java
@@ -1,10 +1,14 @@
+package Day13;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      
-        int[] arr = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
-
+        Scanner z=new Scanner(System.in);
+        int n=z.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=z.nextInt();
+        }
         // Sorting logic
         for(int i = 0; i < arr.length; i++){
             for(int j = i; j < arr.length; j++){
@@ -15,13 +19,11 @@ public class Main {
                 }
             }
         }
-
         // Convert array to List
         List<Integer> list = new ArrayList<>();
         for(int num : arr){
             list.add(num);
         }
-
         // Print as List
         System.out.println(list);
     }
