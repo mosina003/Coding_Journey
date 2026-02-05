07af3b10ed917bd8087fd60fc6402cd01ce26c8f
mosina003 <smosina003@gmail.com>
2026-02-05 23:46:19 +0530

    Update ArrRotatebyK.java

--- Day16/ArrRotatebyK.java
+++ Day16/ArrRotatebyK.java
@@ -3,34 +3,31 @@ package Day16;
 import java.util.*;
 
 public class ArrRotatebyK{
-
-    static void reverse(int[] arr, int l, int r) {
-        while (l < r) {
-            int temp = arr[l];
-            arr[l] = arr[r];
-            arr[r] = temp;
-            l++;
-            r--;
-        }
+    public static void reverse(int[] arr,int l,int r){
+      while(l<r){
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
+      }
     }
-
-    public static void main(String[] args) {
-
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        int k = 2;
-        int n = arr.length;
-
-        k = k % n;
-
-        // Step 1: Reverse first k elements
-        reverse(arr, 0, k - 1);
-
-        // Step 2: Reverse remaining elements
-        reverse(arr, k, n - 1);
-
-        // Step 3: Reverse whole array
-        reverse(arr, 0, n - 1);
-
-        System.out.println(Arrays.toString(arr));
+    public static void main(String[] args){
+      Scanner z=new Scanner(System.in);
+      int n=z.nextInt();
+      int[] arr=new int[n];
+      for(int i=0;i<n;i++){
+        arr[i]=z.nextInt();
+      }
+      int k=z.nextInt();
+      k=k%n;
+      // Step 1: Reverse first k elements
+      reverse(arr,0,k-1);
+      // Step 2: Reverse remaining elements
+      reverse(arr,k,n-1);
+      // Step 3: Reverse whole array
+      reverse(arr,0,n-1);
+      
+      System.out.println(Arrays.toString(arr));
     }
 }
