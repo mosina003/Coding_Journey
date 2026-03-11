9987136ddec4c47bd1df7628a51833194e8e826a
mosina003 <smosina003@gmail.com>
2026-03-11 16:28:20 +0530

    Add Day23 triplet counting and update Day22

--- Day22/triple0.java
+++ Day22/triple0.java
@@ -1,3 +1,36 @@
-public class main {
-    
+package Day22;
+import java.util.*;
+
+public class triple0 {
+  public static boolean tripletzero(int[] arr){
+    int m=arr.length;
+    Arrays.sort(arr);
+    for(int i=0;i<m-2;i++){
+      int left=i+1;
+      int right=m-1;
+  
+      while(left<right){
+        int sum=arr[i]+arr[left]+arr[right];
+        if(sum==0){
+          return true;
+        }
+        else if(sum<0){
+          left++;
+        }
+        else{
+          right--;
+        }
+      }
+    }
+    return false;
+  }
+    public static void main(String[] args) {
+      Scanner z=new Scanner(System.in);
+      int n=z.nextInt();
+      int[] arr=new int[n];
+      for(int i=0;i<n;i++){
+        arr[i]=z.nextInt();
+      }
+      System.out.println(tripletzero(arr));
+    }
 }
